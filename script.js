a7311b9544a5fda59123ad53e309c2f27847d556
christina888 <cechianelli@gmail.com>
2024-11-19 15:02:14 -0600

    pause button

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ resetBtn.addEventListener("click", resetTimer);
 document.getElementById("timer").textContent = "25:00";
 let timeLeft = 25 * 60;
 let timerInterval = null;
+let isPaused = false;
+
+function updateUI() {
+  document.getElementById("timer").textContent = "25:00";
+  let timeLeft = 25 * 60;
+}
 
 function startTimer() {
-  if (timerInterval) {
+  if (timerInterval && isPaused == false) {
     console.log("timer already running");
     return;
   }
@@ -19,10 +25,10 @@ function startTimer() {
   timerInterval = setInterval(() => {
     let minutes = Math.floor(timeLeft / 60);
     let seconds = timeLeft % 60;
-
-    let displayMinutes = `${String(minutes).padStart(2, "0")}`;
-    let displaySeconds = `${String(seconds).padStart(2, "0")}`;
-    let updatedDisplay = displayMinutes + ":" + displaySeconds;
+    let updatedDisplay =
+      `${String(minutes).padStart(2, "0")}` +
+      ":" +
+      `${String(seconds).padStart(2, "0")}`;
     document.getElementById("timer").textContent = updatedDisplay;
     console.log(updatedDisplay);
 
@@ -30,18 +36,23 @@ function startTimer() {
 
     if (timeLeft < 0) {
       clearInterval(timerInterval);
+      resetTimer();
     }
   }, 1000);
+  console.log("start timer");
 }
 
 function pauseTimer() {
-  console.log("pause timer");
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    isPaused = true;
+    console.log("pause timer");
+  }
 }
 
 function resetTimer() {
   clearInterval(timerInterval);
   timerInterval = null;
-  document.getElementById("timer").textContent = "25:00";
-  timeLeft = 25 * 60;
+  updateUI();
   console.log("reset timer");
 }
